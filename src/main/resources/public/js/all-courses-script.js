73b950abf67afa6d677f09cb8a64a45e25964850
Gergana Tsvetkova <gerytsv@gmail.com>
2022-01-26 17:30:55 +0200

    fix navbar on profile page

--- src/main/resources/public/js/all-courses-script.js
+++ src/main/resources/public/js/all-courses-script.js
@@ -1,25 +1,25 @@
 
-document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function (event) {
 	const container = document.getElementById("courses-container");
 	console.log(container);
 	//TODO: images, names, times to be get from backend
-	const names=["Borderlands 2", "Draconball Z", "Zelda", "Ghost Recon", "Sims4", "Tearaway", "Age of Empires II", "The Witcher", "Past Cure", "Red Dead Redemption II", "Call of Duty", "Final Fantasy VII", "Doom", "FarmVille", "Jedi", "Persona5", "Bloodhorne", "Daysgone", "Daysgone II", "NBA2K20", "Killzone", "Mortal Kombat II", "Outer Worlds", "Fallout 4"];
+	const names = ["Borderlands 2", "Draconball Z", "Zelda", "Ghost Recon", "Sims4", "Tearaway", "Age of Empires II", "The Witcher", "Past Cure", "Red Dead Redemption II", "Call of Duty", "Final Fantasy VII", "Doom", "FarmVille", "Jedi", "Persona5", "Bloodhorne", "Daysgone", "Daysgone II", "NBA2K20", "Killzone", "Mortal Kombat II", "Outer Worlds", "Fallout 4"];
 	const type = "CSS";
-    const durationHours = "2 часа";
-    const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg"];
+	const durationHours = "2 часа";
+	const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg"];
 	const description = "Това е най-добрия курс за CSS";
 
-	function makeRows(rows,cols){
+	function makeRows(rows, cols) {
 		container.style.setProperty('--grid-rows', rows);
 		container.style.setProperty('--grid-cols', cols);
-	
-		for(i = 0; i < (rows*cols); i++){
+
+		for (let i = 0; i < (rows * cols); i++) {
 
 			//course - div (container card)
-            let courseDiv = document.createElement('div');
-            courseDiv.className='course-card';
+			let courseDiv = document.createElement('div');
+			courseDiv.className = 'course-card';
 
-            //course - name
+			//course - name
 			let courseName = document.createElement('h3');
 			let _name = document.createTextNode(names[i].toUpperCase());
 			courseName.appendChild("Gosho");
@@ -27,17 +27,17 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 			//course - img
 			let courseImg = document.createElement('img');
-			courseImg.src="https://vkusnoipolezno.bg/wp-content/uploads/2021/01/sladko-ot-qgodi-buzko.jpg";
+			courseImg.src = "https://vkusnoipolezno.bg/wp-content/uploads/2021/01/sladko-ot-qgodi-buzko.jpg";
 			console.log(courseImg.src);
 			courseDiv.appendChild(courseImg);
 
 			let courseDuration = document.createElement('p');
-            //let timeText = document.createTextNode(times[i]);
-            courseDuration.appendChild(durationHours);
-            courseDiv.appendChild(courseDuration);
+			//let timeText = document.createTextNode(times[i]);
+			courseDuration.appendChild(durationHours);
+			courseDiv.appendChild(courseDuration);
 
-            let courseLink =  document.createElement('a');
-            courseLink.href ="course-screen.html";
+			let courseLink = document.createElement('a');
+			courseLink.href = "course-screen.html";
 
 			let eventTime = document.createElement('p');
 			let timeText = document.createTextNode(times[i]);
@@ -54,5 +54,5 @@ document.addEventListener("DOMContentLoaded", function(event){
 		};
 	};
 
-	makeRows(6,4);
+	makeRows(6, 4);
 });
